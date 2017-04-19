ed495eeeee62af14c2ffd914c0903ec88d8ce7dc
Charles Adeeko <cadeeko@googlemail.com>
2017-04-19 21:53:18 +0100

    chore(Basic-Algorithm-Scripting): finished longest word in a string exercise

---
+++ Javascript-Challenges/Basic-Algorithm-Scripting/find_the_Longest_Word_In_a_string.js
@@ -0,0 +1,51 @@
+/**
+ * Created by charlesadeeko on 18/04/2017.
+ */
+
+/** Challenge
+ *
+ * Return the length of the longest word in the provided sentence.
+ * Your response should be a number.
+ *
+ */
+
+// 1st Draft
+function findLongestWord(str) {
+
+    var wordArray;
+    var numberArray = [];
+    var largestNumber = 0;
+
+
+    //Split received words into an array
+    wordArray = str.split(" ");
+
+    //Count and store each word length in a new array.
+    for (i = 0; i < wordArray.length; i++) {
+
+        // store word number in an variable
+        var test = wordArray[i].length;
+
+        // push variable number into a new array
+        numberArray.push(test);
+        }
+
+    // loop through number array to find largest number
+    for (i = 0; i < numberArray.length; i++) {
+        if(numberArray[i] > largestNumber) {
+            largestNumber = numberArray[i];
+        }
+    }
+
+    // str = largest number in array
+    str = largestNumber;
+
+    return str;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
+findLongestWord("The quick brown fox jumped over the lazy dog");
+findLongestWord("May the force be with you");
+findLongestWord("Google do a barrel roll");
+findLongestWord("What is the average airspeed velocity of an unladen swallow");
+findLongestWord("What if we try a super-long word such as otorhinolaryngology");
