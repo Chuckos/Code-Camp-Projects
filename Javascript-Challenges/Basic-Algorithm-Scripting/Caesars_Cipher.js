07cbf1d61b55cd6650f970b3e3f3ce73d899db00
Charles Adeeko <cadeeko@googlemail.com>
2017-08-29 18:36:40 +0100

    updated tests

--- Javascript-Challenges/Basic-Algorithm-Scripting/Caesars_Cipher.js
+++ Javascript-Challenges/Basic-Algorithm-Scripting/Caesars_Cipher.js
@@ -16,11 +16,31 @@
 
 function rot13(str) { // LBH QVQ VG!
 
+    // Usee string.charcodeAt() to convert the English character to ASCII
+    var string = str;
+    var storeArry = [];
+
+    for (var i = 0; i < string.length; i++) {
+       var index = string.charCodeAt([i]);
+       storeArry.push(index);
+    }
+
+    for (var i = 0; i < storeArry.length; i++) {
+        if ()
+    }
+
+    console.log("information" + storeArry);
+
+
+    //Use string.fromCharCode() to convert ASCII to English Character.
+
+    //Leave anything that doesn't come between A-Z as it is.
+
     return str;
 }
 
 // Change the inputs below to test
 rot13("SERR PBQR PNZC");                                //should decode to "FREE CODE CAMP"
-rot13("SERR CVMMN!");                                   //should decode to "FREE PIZZA!"
+/*rot13("SERR CVMMN!");                                   //should decode to "FREE PIZZA!"
 rot13("SERR YBIR?");                                    //should decode to "FREE LOVE?"
-rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK."); //should decode to "THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX."
+rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.");*/ //should decode to "THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX."
