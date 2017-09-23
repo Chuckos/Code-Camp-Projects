2d3d241bff3fd72c87430813477b201ec36d97ed
Charles Adeeko <cadeeko@googlemail.com>
2017-09-23 15:49:24 +0100

    test(caesars_Cipher): refactored code.

--- Javascript-Challenges/Basic-Algorithm-Scripting/Caesars_Cipher.js
+++ Javascript-Challenges/Basic-Algorithm-Scripting/Caesars_Cipher.js
@@ -14,33 +14,34 @@
  */
 
 
-function rot13(str) { // LBH QVQ VG!
+function rot13(str) {
 
-    // Usee string.charcodeAt() to convert the English character to ASCII
-    var string = str;
-    var storeArry = [];
+    // Split str into a character array
+    return str.split('')
 
-    for (var i = 0; i < string.length; i++) {
-       var index = string.charCodeAt([i]);
-       storeArry.push(index);
-    }
+    //Iterate over each cahracter in the arry
+    .map.call(str, function(char){
 
-    for (var i = 0; i < storeArry.length; i++) {
-        if ()
-    }
+        //convert char to a character code
+        x = char.charCodeAt(0);
 
-    console.log("information" + storeArry);
+        //Checks if character lies between A-Z
+        if (x < 65 || x > 90) {
+            return String.fromCharCode(x); // Return un-converted character
+        }
 
+        // N = ASCII 78, if the character code is less than 78, shift forward 13 places
+        else if (x < 78) {
+            return String.fromCharCode(x +13);
+        }
+        // otherwise shift the character 13 places  backward
+        return String.fromCharCode(x - 13);
+    }).join(''); // rejoin the array into a string
 
-    //Use string.fromCharCode() to convert ASCII to English Character.
-
-    //Leave anything that doesn't come between A-Z as it is.
-
-    return str;
 }
 
 // Change the inputs below to test
 rot13("SERR PBQR PNZC");                                //should decode to "FREE CODE CAMP"
-/*rot13("SERR CVMMN!");                                   //should decode to "FREE PIZZA!"
+rot13("SERR CVMMN!");                                   //should decode to "FREE PIZZA!"
 rot13("SERR YBIR?");                                    //should decode to "FREE LOVE?"
-rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.");*/ //should decode to "THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX."
+rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK."); //should decode to "THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX."
